3d52cdf2375041618b4d21a4376b2a450b5fb037
Nazkiii <113229345+Nazkiii@users.noreply.github.com>
2022-09-10 14:35:41 +0700

    Update Header.lua

--- Header.lua
+++ Header.lua
@@ -1,22 +1,22 @@
 bot_raw = {}
 ---======================---
-bot_raw["NAMEBOT1"] = {
+bot_raw["Qweal28"] = {
 webhooks = {
-  ["LINK"]  = '',
+  ["LINK"]  = 'https://discord.com/api/webhooks/1013450963442483220/StamAuc8VHvFDn8QlrMFmdGYK8A1Gsx-qy4DNLpB7JUbs52NBufTlaPz3bBtUD0pcyq6',
   ["EDIT"]  = false
 },
 -----------------------
 bot_config = {
-  ["World"] = {"FARM1"},
-  ["Door"] = 'FARM_ID',
+  ["World"] = {"EGHMQ"},
+  ["Door"] = 'KONTOLABIS',
   ["Loop"] = true,
   ["Save_Mode"] = 1,
   ["Save"] = {
               {
-                ["World"] = "World_Save",
-                ["Door"] = "Door_ID",       --Mode 1
-                ["Path_Seed"] = 20,
-                ["Path_Pack"] = 12
+                ["World"] = "MAHASAVER",
+                ["Door"] = "KONTOLABIS",       --Mode 1
+                ["Path_Seed"] = 3004,
+                ["Path_Pack"] = 14
               },
               {
                 ["World_Pack"] = "World_Save_Pack",
@@ -34,29 +34,29 @@ bot_mode = {
   ["Block"] = 5666,
   ["Type"] = 'fg',
   ["Ignore Gems"] = false, --Ignore Gems When Harvesting
-  ["Coordinate_Break"] = {2,1}
+  ["Coordinate_Break"] = {3,2}
 },
 ------------------------
 bot_environment = {
   ["Trash"] = {
               ["Limit"] = 120,
-              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162}
+              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162,1098,1058,1096,1094}
               },
   ["Buy"] = {
             ["Mode"] = true,
             ["Name"] = 'world_lock',
             ["Price"] = 2000,
-            ["Item_List"] = {242},
-            ["Limit"] = 3,
+            ["Item_List"] = {242,1828},
+            ["Limit"] = 10,
             ["When"] = {
                         ["Harvest"] = true,
                         ["Plant"]   = true,
                         ["Break"]   = true
                        }
             },
-  ["Jammer"] = true,
+  ["Jammer"] = false,
   ["Ban"] = {
-            ["Mode"] = true,
+            ["Mode"] = false,
             ["Ignore"] = {"Ignored_Name"}
             },
   ["Leave"] = {
@@ -67,69 +67,3 @@ bot_environment = {
 }
 }
 
-bot_raw["NAMEBOT2"] = {
-  webhooks = {
-  ["LINK"]  = '',
-  ["EDIT"]  = false
-},
------------------------
-bot_config = {
-  ["World"] = {"FARM1"},
-  ["Door"] = 'FARM_ID',
-  ["Loop"] = true,
-  ["Save_Mode"] = 1,
-  ["Save"] = {
-              {
-                ["World"] = "World_Save",
-                ["Door"] = "Door_ID",       --Mode 1
-                ["Path_Seed"] = 20,
-                ["Path_Pack"] = 12
-              },
-              {
-                ["World_Pack"] = "World_Save_Pack",
-                ["Door_Pack"] = "Door_ID_Save_Pack",
-                ["Path_Pack"] = 12,
-                                                      --Mode 2
-                ["World_Seed"] = "World_Save_Seed",     
-                ["Door_Seed"] = "Door_ID_Save_Seed",
-                ["Path_Seed"] = 20
-              }
-             }
-},
-------------------------
-bot_mode = {
-  ["Block"] = 5666,
-  ["Type"] = 'fg',
-  ["Ignore Gems"] = false, --Ignore Gems When Harvesting
-  ["Coordinate_Break"] = {2,1}
-},
-------------------------
-bot_environment = {
-  ["Trash"] = {
-              ["Limit"] = 120,
-              ["List"] = {5040,5042,5044,5032,5034,5036,5038,5024,5026,5028,5030,7164,7162}
-              },
-  ["Buy"] = {
-            ["Mode"] = true,
-            ["Name"] = 'world_lock',
-            ["Price"] = 2000,
-            ["Item_List"] = {242},
-            ["Limit"] = 3,
-            ["When"] = {
-                        ["Harvest"] = true,
-                        ["Plant"]   = true,
-                        ["Break"]   = true
-                       }
-            },
-  ["Jammer"] = true,
-  ["Ban"] = {
-            ["Mode"] = true,
-            ["Ignore"] = {"Ignored_Name"}
-            },
-  ["Leave"] = {
-            ["Mode"] = true,
-            ["Limit"] = 5,
-            ["Delay"] = 30 --Second
-              }
-}
-}
